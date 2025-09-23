c705573d78698fe96b5e2731be80acfbfc216215
dtyrrelldev <dtyrrell@ualberta.ca>
2025-09-23 17:38:25 -0600

    Made Shape class

---
+++ app/src/main/java/com/example/haboob/Shape.java
@@ -0,0 +1,7 @@
+package com.example.haboob;
+// Create a model class (ex. circle, rectangle, star, etc) (new java file)
+// that extends Shape in your own branch.
+ public abstract class  Shape {
+     private int x;
+     private int y;
+}
